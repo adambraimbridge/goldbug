e1bc42b84c7eb6da29080496f6d64fb21c54b44a
Adam Braimbridge <adam@braimbridge.com>
2019-04-28 08:24:12 +0100

    Rebuilding as an express app

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
-const PouchDB = require("pouchdb");
-const express = require("express");
-const app = express();
+const PouchDB = require('pouchdb')
+const express = require('express')
+const app = express()
 
-app.use("/db", require("express-pouchdb")(PouchDB));
-app.use("/", require("")
+app.use(
+	'/database',
+	require('express-pouchdb')(PouchDB.defaults({ prefix: './database/' }))
+)
+app.use('/', require('./router'))
 
-const $port = process.env.PORT || 3000;
-app.listen($port, () => {
-  console.log(`Running on http://localhost:${process.env.PORT}`);
-});
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+	console.log(`Running on http://localhost:${process.env.PORT}`)
+})
