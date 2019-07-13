bcd05ba37e39f4036c4b7b9f91ef35d8aec02827
Adam Braimbridge <adam.braimbridge@ft.com>
2019-07-13 11:56:19 +0100

    saturday

--- public/js/main.js
+++ public/js/main.js
@@ -1,108 +1,52 @@
-;(function() {
-	'use strict'
-	var model = null
-	var sanitize = function(id) {
-		return id.replace(/[:.]/gi, '-')
+;(() => {
+	const sleep = milliseconds => {
+		if (!milliseconds) milliseconds = Math.floor(Math.random() * (3000 - 1000)) + 1000
+		return new Promise(resolve => setTimeout(resolve, milliseconds))
 	}
 
-	var addToPuzzle = function(docs) {
-		for (var i = 0; i < docs.length; i++) {
-			var doc = docs[i]
-
-			var isPuzzle = doc.type === 'puzzle' || doc._id.indexOf('puzzle:') === 0
-			var puzzles = null
-
-			if (isPuzzle) {
-				puzzles = document.getElementById('puzzle')
-			} else {
-				continue
-			}
-
-			doc._sanitizedid = sanitize(doc._id)
-
-			var template = document.getElementById('puzzle-template').innerHTML
-			template = template.replace(/\{\{(.+?)\}\}/g, function($0, $1) {
-				var fields = $1.split('.')
-				var value = doc
-				while (fields.length) {
-					if (value.hasOwnProperty(fields[0])) {
-						value = value[fields.shift()]
-					} else {
-						value = null
-						break
-					}
-				}
-				return value || ''
-			})
-
-			var puzzlediv = document.createElement('div')
-			puzzlediv.id = doc._sanitizedid
-			puzzlediv.className = 'card collapsible'
-			puzzlediv.innerHTML = template
-
-			var existingdiv = document.getElementById(doc._sanitizedid)
-			if (existingdiv) {
-				puzzles.replaceChild(puzzlediv, existingdiv)
-			} else {
-				puzzles.insertBefore(puzzlediv, puzzles.firstChild)
-			}
+	const sendDelayedMessages = async onboardingMessages => {
+		for (let { name, content } of onboardingMessages) {
+			await sleep()
+			console.log(`${name} is typing ... `)
+			await sleep(content.length * 100)
+			console.log(content)
 		}
 	}
 
-	var player = function(themodel) {
-		if (themodel) {
-			themodel(function(err, response) {
-				if (err) {
-					console.error(err)
-				} else {
-					model = response
-					model.puzzles(function(err, docs) {
-						if (err) {
-							console.error(err)
-						} else {
-							addToPuzzle(docs, true)
-						}
-						console.log('Player is ready.')
-					})
-				}
-			})
-		}
-		return this
+	const initialise = async () => {
+		const adventure = await database.get('adventure')
+		console.dir(adventure)
+		await sleep()
+		document.getElementById('header-content').innerHTML = 'The Ministry | Secure Channel #SC-836.20.2'
 	}
 
-	player.openadd = function() {
-		var form = document.getElementById('puzzle-add')
-		form.reset()
-		document.body.className += ' ' + form.id
+	const onboard = async () => {
+		const onboardingMessages = [
+			{
+				name: 'Agent Selaginel',
+				content: 'Good morning.',
+			},
+			{
+				name: 'Agent Selaginel',
+				content: 'As per usual protocol we must first confirm your identification.',
+			},
+			{
+				name: 'Agent Selaginel',
+				content: "Once you've indicated your codename we may proceed.",
+			},
+		]
+		await sendDelayedMessages(onboardingMessages)
+		await sleep(1000)
+		document.getElementById('codename-ui').classList.remove('hidden')
 	}
 
-	player.closeadd = function() {
-		document.body.className = document.body.className.replace('puzzle-add', '').trim()
+	const run = async () => {
+		// Initialise
+		await initialise()
+
+		// If appropriate, do onboarding
+		await onboard()
 	}
 
-	player.add = function(event) {
-		var form = event.target
-		var elements = form.elements
-		var doc = {}
-
-		for (var i = 0; i < elements.length; i++) {
-			if (elements[i].tagName.toLowerCase() !== 'button') {
-				doc[elements[i].name] = elements[i].value
-			}
-		}
-
-		model.save(doc, function(err, updated) {
-			if (err) {
-				console.error(err)
-			} else {
-				doc._id = doc._id || updated._id || updated.id
-				addToPuzzle([doc])
-				player.closeadd()
-			}
-		})
-	}
-
-	window.player = player
-
-	
+	window.addEventListener('DOMContentLoaded', run)
 })()
--- server.js
+++ server.js
@@ -3,16 +3,13 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use('/js', express.static(path.join(__dirname, 'public/js')))
-app.use('/css', express.static(path.join(__dirname, 'public/css')))
-app.use('/favicons', express.static(path.join(__dirname, 'public/favicons')))
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public/index.html'))
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
 // Run
 app.listen(port, () => {
-	console.log(`Running on http://localhost:${port}`)
+  console.log(`Running on http://localhost:${port}`)
 })
