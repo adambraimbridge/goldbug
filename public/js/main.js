a779ed4032793709b1635244ee6eaaea0cd5a56c
Adam Braimbridge <adam.braimbridge@ft.com>
2019-08-27 21:23:53 +0100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 import 'https://cdn.jsdelivr.net/npm/pouchdb@7.0.0/dist/pouchdb.min.js'
 import 'https://cdn.jsdelivr.net/npm/pouchdb@7.0.0/dist/pouchdb.find.min.js'
-import 'netlify-identity-widget'
+import '../../node_modules/netlify-identity-widget'
 
 const DB_HOST = 'https://658ec9bf-a8e5-40e2-8fae-4c14ca03b65b-bluemix.cloudantnosqldb.appdomain.cloud'
 const ORGANIZATION_TITLE = 'The Ministry of Information'
