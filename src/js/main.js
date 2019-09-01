46e47de56762a6ac29646898c8610dbe62e847d2
Adam Braimbridge <adam.braimbridge@ft.com>
2019-09-01 22:02:32 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -19,14 +19,14 @@ this.clearHash()
 
 const params = Map(hashParams.entries())
 
-const validNonce = validateNonce(params.get('state'))
-if (!validNonce) {
-	return cb(new Error('Invalid nonce'))
-}
+// const validNonce = validateNonce(params.get('state'))
+// if (!validNonce) {
+// 	return cb(new Error('Invalid nonce'))
+// }
 
-if (params.has('error')) {
-	return cb(new Error(`${params.get('error')}: ${params.get('error_description')}`))
-}
+// if (params.has('error')) {
+// 	return cb(new Error(`${params.get('error')}: ${params.get('error_description')}`))
+// }
 
 if (params.has('access_token')) {
 	const { access_token: token, ...data } = params.toJS()
