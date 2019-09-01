510cd2bc13756d2d42c25850797e87b3ae3ef6e9
Adam Braimbridge <adam.braimbridge@ft.com>
2019-09-01 21:33:59 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,29 @@ const auth = new GoTrue({
 })
 const user = auth.currentUser()
 console.log({ user })
+
+/**
+ * Complete authentication if we were redirected back to from the provider.
+ */
+const hashParams = new URLSearchParams(document.location.hash.replace(/^#?\/?/, ''))
+if (!hashParams.has('access_token') && !hashParams.has('error')) {
+	return
+}
+// Remove tokens from hash so that token does not remain in browser history.
+this.clearHash()
+
+const params = Map(hashParams.entries())
+
+const validNonce = validateNonce(params.get('state'))
+if (!validNonce) {
+	return cb(new Error('Invalid nonce'))
+}
+
+if (params.has('error')) {
+	return cb(new Error(`${params.get('error')}: ${params.get('error_description')}`))
+}
+
+if (params.has('access_token')) {
+	const { access_token: token, ...data } = params.toJS()
+	console.log({ access_token, data })
+}
