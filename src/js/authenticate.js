5bfcd6c90a53bb252616c02a05da0c13c2ddb289
Adam Braimbridge <adam.braimbridge@ft.com>
2019-09-02 18:09:28 +0100

    Update authenticate.js

--- src/js/authenticate.js
+++ src/js/authenticate.js
@@ -20,8 +20,8 @@ const getAuthenticationData = hash => {
 			return result
 		}, {})
 
-	// Remove the token so it's not visible in the URL
-	document.location.hash = ''
+	// Remove tokens from hash so that token does not remain in browser history.
+	history.replaceState(null, null, '/')
 	return authenticationData
 }
 
@@ -42,17 +42,6 @@ const getAppList = accessToken => {
 }
 
 export const checkAuthentication = async () => {
-	/*
-	 * The Oauth2 implicit grant flow works by sending the user to Netlify where she'll
-	 * be asked to grant authorization to your application. Netlify will then redirect
-	 * back to the Redirect URI on file for your app and set an access_token paramter
-	 * in the "hash" part of the URL.
-	 *
-	 * If we have any hash, it's because the user is coming back from Netlify and we
-	 * can start doing API requests on their behalf.
-	 *
-	 * If not, we'll trigger the first step and prepare to send the user to Netlify.
-	 */
 	const hash = document.location.hash
 	if (hash) {
 		const authenticationData = getAuthenticationData(hash)
@@ -61,11 +50,9 @@ export const checkAuthentication = async () => {
 			return false
 		}
 	} else {
-		const clientId = 'https://www.goldbug.club'
 		const redirectURI = document.location.href
 		const nonce = createNonce()
-		const href = `https://app.netlify.com/authorize?client_id=${clientId}&redirect_uri=${redirectURI}&state=${nonce}&response_type=token`
-
+		const href = `https://www.goldbug.club/.netlify/identity/authorize?provider=google&redirect_uri=${redirectURI}&state=${nonce}&response_type=token`
 		console.log({ href })
 	}
 }
