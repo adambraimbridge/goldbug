67299bd7cd988d0828572c68810b6af9859618dd
Adam Braimbridge <adam.braimbridge@ft.com>
2019-09-02 14:51:36 +0100

    Update authenticate.js

--- src/js/authenticate.js
+++ src/js/authenticate.js
@@ -1,37 +1,34 @@
 const oauthURL = 'https://www.goldbug.club/.netlify/identity/authorize?provider=google'
 
-export async const checkAuthentication = () => {
+export const checkAuthentication = async () => {
+	// Hash parameters are like query parameters, except with a `#` instead of a `?`
+	const hashParams = new URLSearchParams(document.location.hash.replace(/^#?\/?/, ''))
 
-  // Hash parameters are like query parameters, except with a `#` instead of a `?`
-  const hashParams = new URLSearchParams(
-    document.location.hash.replace(/^#?\/?/, '')
-  )
+	// // If there's no access_token hash parameter, redirect to the oauth URL for authentication
+	// if (!hashParams.has('access_token')) {
+	//   return {
+	//     statusCode: 301, // redirect
+	//     headers: {
+	//       Location: oauthURL,
+	//     },
+	//   }
+	// }
 
-  // // If there's no access_token hash parameter, redirect to the oauth URL for authentication
-  // if (!hashParams.has('access_token')) {
-  //   return {
-  //     statusCode: 301, // redirect
-  //     headers: {
-  //       Location: oauthURL,
-  //     },
-  //   }
-  // }
+	// Remove tokens from hash so that token does not remain in browser history.
+	history.replaceState(null, null, '/')
 
-  // Remove tokens from hash so that token does not remain in browser history.
-  history.replaceState(null, null, '/')
-
-  const params = new Map(hashParams.entries())
-  if (params.has('error')) {
-		console.error(`${ params.get('error') }: ${params.get('error_description')}`)
+	const params = new Map(hashParams.entries())
+	if (params.has('error')) {
+		console.error(`${params.get('error')}: ${params.get('error_description')}`)
 		// return {
-    //   statusCode: 401, // Unauthorized
-      // body: `Failed to Authenticate. ${params.get('error')}: ${params.get(
-      //   'error_description'
-      // )}`,
-    // }
-  }
+		//   statusCode: 401, // Unauthorized
+		// body: `Failed to Authenticate. ${params.get('error')}: ${params.get(
+		//   'error_description'
+		// )}`,
+		// }
+	}
 
-  const { access_token: token, ...data } = params.toJS()
+	const { access_token: token, ...data } = params.toJS()
 
-  console.log({ access_token, data })
+	console.log({ access_token, data })
 }
