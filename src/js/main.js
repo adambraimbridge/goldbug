34724b5f31960a36f1bf8aeda05912d2e0fa8984
Adam Braimbridge <adam.braimbridge@ft.com>
2019-09-02 19:46:20 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,36 @@
 import GoTrue from 'gotrue-js'
 import { checkAuthentication } from './authenticate'
-
-const auth = new GoTrue({
-	APIUrl: 'https://www.goldbug.club/.netlify/identity',
-	setCookie: true,
-})
-
-const settings = await auth.settings()
-const user = auth.getUserData()
-
-console.log({ user, settings })
-
-const authenticationData = checkAuthentication()
-if (authenticationData) {
-	localStorage.setItem(gotrue.user, authenticationData)
-	console.log({ authenticationData })
-	const list = getAppList(authenticationData.access_token)
-	console.log({ list })
-}
-
-const getAppList = accessToken => {
-	fetch('https://api.netlify.com/api/v1/sites', {
-		headers: {
-			Authorization: 'Bearer ' + accessToken,
-		},
+;(async () => {
+	const auth = new GoTrue({
+		APIUrl: 'https://www.goldbug.club/.netlify/identity',
+		setCookie: true,
 	})
-		.then(response => response.json())
-		.then(json => {
-			console.log('Your sites: ' + json.map(site => `<a href="${site.url}">${site.url}</a>`).join(','))
+
+	const settings = await auth.settings()
+	const user = auth.getUserData()
+
+	console.log({ user, settings })
+
+	const authenticationData = await checkAuthentication()
+	if (authenticationData) {
+		localStorage.setItem('adam@braimbridge.com', authenticationData)
+		console.log({ authenticationData })
+		const list = getAppList(authenticationData.access_token)
+		console.log({ list })
+	}
+
+	const getAppList = accessToken => {
+		fetch('https://api.netlify.com/api/v1/sites', {
+			headers: {
+				Authorization: 'Bearer ' + accessToken,
+			},
 		})
-		.catch(error => {
-			console.log(`Error fetching sites: ${error}`)
-		})
-}
+			.then(response => response.json())
+			.then(json => {
+				console.log('Your sites: ' + json.map(site => `<a href="${site.url}">${site.url}</a>`).join(','))
+			})
+			.catch(error => {
+				console.log(`Error fetching sites: ${error}`)
+			})
+	}
+})()
