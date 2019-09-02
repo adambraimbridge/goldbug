f45e5c261f1d8af1fefdec7a1d7c16726cf20b63
Adam Braimbridge <adam.braimbridge@ft.com>
2019-09-02 13:58:24 +0100

    auth

--- functions/authenticate.js
+++ functions/authenticate.js
@@ -1,38 +1,37 @@
-const oauthURL =
-  'https://www.goldbug.club/.netlify/identity/authorize?provider=google'
+const oauthURL = 'https://www.goldbug.club/.netlify/identity/authorize?provider=google'
 
 exports.handler = async ({ httpMethod, body, ...other }) => {
-  console.log({ httpMethod, body, other })
-
-  // Hash parameters are like query parameters, except with a `#` instead of a `?`
-  const hashParams = new URLSearchParams(
-    document.location.hash.replace(/^#?\/?/, '')
-  )
-
-  // If there's no access_token hash parameter, redirect to the oauth URL for authentication
-  if (!hashParams.has('access_token')) {
-    return {
-      statusCode: 301, // redirect
-      headers: {
-        Location: oauthURL,
-      },
-    }
-  }
-
-  // Remove tokens from hash so that token does not remain in browser history.
-  history.replaceState(null, null, '/')
-
-  const params = new Map(hashParams.entries())
-  if (params.has('error')) {
-    return {
-      statusCode: 401, // Unauthorized
-      body: `Failed to Authenticate. ${params.get('error')}: ${params.get(
-        'error_description'
-      )}`,
-    }
-  }
-
-  const { access_token: token, ...data } = params.toJS()
-
-  console.log({ access_token, data })
+	console.log({ httpMethod, body, other })
 }
+//   // Hash parameters are like query parameters, except with a `#` instead of a `?`
+//   const hashParams = new URLSearchParams(
+//     document.location.hash.replace(/^#?\/?/, '')
+//   )
+
+//   // If there's no access_token hash parameter, redirect to the oauth URL for authentication
+//   if (!hashParams.has('access_token')) {
+//     return {
+//       statusCode: 301, // redirect
+//       headers: {
+//         Location: oauthURL,
+//       },
+//     }
+//   }
+
+//   // Remove tokens from hash so that token does not remain in browser history.
+//   history.replaceState(null, null, '/')
+
+//   const params = new Map(hashParams.entries())
+//   if (params.has('error')) {
+//     return {
+//       statusCode: 401, // Unauthorized
+//       body: `Failed to Authenticate. ${params.get('error')}: ${params.get(
+//         'error_description'
+//       )}`,
+//     }
+//   }
+
+//   const { access_token: token, ...data } = params.toJS()
+
+//   console.log({ access_token, data })
+// }
