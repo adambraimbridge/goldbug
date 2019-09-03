54fb6083d2d0639299dec41bf8794201c3f3e5e2
Adam Braimbridge <adam.braimbridge@ft.com>
2019-09-03 17:57:07 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -9,5 +9,7 @@ import { checkAuthentication } from './authenticate'
 	const authenticationData = await checkAuthentication()
 	if (authenticationData) {
 		const user = await auth.createUser(authenticationData, true).catch(console.error)
+
+		// If the user does not have Cloudant credentials in their metadata, create a new DB for that user.
 	}
 })()
