60ce102b4690003f9ac8c05ab174a5b1c0fb0358
Adam Braimbridge <adam.braimbridge@ft.com>
2019-09-11 22:07:51 +0100

    I still hate classes

--- src/components/app.js
+++ src/components/app.js
@@ -1,32 +1,31 @@
-import { h, Component } from 'preact';
-import { Router } from 'preact-router';
+import { createElement, Component, useState } from 'preact'
+import { Router } from 'preact-router'
 
-import Header from './header';
+import Header from './header'
 
 // Code-splitting is automated for routes
-import Home from '../routes/home';
-import Profile from '../routes/profile';
+import Home from '../routes/home'
+import Profile from '../routes/profile'
 
-export default class App extends Component {
-	
-	/** Gets fired when the route changes.
-	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-	 *	@param {string} event.url	The newly routed URL
-	 */
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
+/** Gets fired when the route changes.
+ *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
+ *	@param {string} event.url	The newly routed URL
+ */
+const handleRoute = e => {
+	const currentUrl = e.url
+}
 
+export default {
 	render() {
 		return (
 			<div id="app">
 				<Header />
-				<Router onChange={this.handleRoute}>
+				<Router onChange={handleRoute}>
 					<Home path="/" />
 					<Profile path="/profile/" user="me" />
 					<Profile path="/profile/:user" />
 				</Router>
 			</div>
-		);
-	}
+		)
+	},
 }
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,4 +1,4 @@
-import { h } from 'preact'
+import { createElement } from 'preact'
 import { Link } from 'preact-router/match'
 import style from './style'
 
