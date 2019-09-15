1a66be1ec520d875bcee0e8d68c192b8570e4a64
Adam Braimbridge <adam.braimbridge@ft.com>
2019-09-15 21:31:55 +0100

    Update index.js

--- src/components/authentication/index.js
+++ src/components/authentication/index.js
@@ -1,3 +1,5 @@
+import { useState, useCallback } from 'preact/hooks'
+
 /**
  * Authenticate against Google via Netlify.
  */
@@ -45,79 +47,68 @@ const getAuthenticatedUser = () => {
 	return currentUser
 }
 
-const UserComponent = () => {
-	const handleAuthentication = () => {
-		if (authenticatedUser) {
-			// Todo: Wire this up to setState() to trigger a render of the component
-			authenticatedUser
+const userComponent = () => {
+	// updateUser() is triggered by the Sign In / Sign Out button.
+	const updateUser = useCallback(() => {
+		const [localUser] = useState()
+		if (localUser) {
+			// User exists, so they must have clicked "Sign Out"
+			localUser
 				.logout()
 				.then(console.log('Signed out.'))
 				.catch(console.error)
+
+			setValue({ localUser: {}, buttonText: 'Sign In', updateUser }), [localUser]
 		} else {
-			// Redirect to OAuth endpoint to sign in
+			// User does not exist, so they must have clicked "Sign In"
+			// Redirect to OAuth endpoint to sign in.
 			location = 'https://www.goldbug.club/.netlify/identity/authorize?provider=google'
 		}
-	}
-	const UserMeta = () => {
-		if (!authenticatedUser) return false
+	})
 
-		const { avatar_url, full_name } = authenticatedUser.user_metadata
-		return (
-			<Fragment>
-				<figure class="media-left image is-24x24">
-					<img class="is-rounded" src={avatar_url} />
-				</figure>
-				<div class="media-content">{full_name}</div>
-			</Fragment>
-		)
+	// Return details to whoever called useState(userComponent)
+	const [localUser, setValue] = useState()
+	if (!localUser) {
+		getAuthenticatedUser()
+			.then(authenticatedUser => {
+				return { localUser: authenticatedUser, buttonText: 'Sign Out', updateUser }
+			})
+			.catch(console.error)
 	}
+	return { localUser, buttonText: 'Google Sign In', updateUser }
+}
 
-	const UserUI = () => {
-		const buttonText = authenticatedUser ? 'Sign Out' : 'Sign In'
-		return (
-			<div class="media-right">
-				<button class="button is-small" onClick={handleAuthentication}>
-					{buttonText}
-				</button>
-			</div>
-		)
-	}
-
-	const authenticatedUser = getAuthenticatedUser()
+const UserMeta = () => {
+	const [{ localUser, buttonText, updateUser }] = useState(userComponent)
+	const { avatar_url, full_name } = localUser.user_metadata
 	return (
-		<div class="media">
-			<UserMeta />
-			<UserUI />
+		<Fragment>
+			<figure class="media-left image is-24x24">
+				<img class="is-rounded" src={avatar_url} />
+			</figure>
+			<div class="media-content">{full_name}</div>
+		</Fragment>
+	)
+}
+
+const UserUI = () => {
+	const [{ buttonText, updateUser }] = useState(userComponent)
+	return (
+		<div class="media-right">
+			<button class="button is-small" onClick={updateUser}>
+				{buttonText}
+			</button>
 		</div>
 	)
 }
-// =============================================================
-// const initialValue = {}
 
-// const userComponent = () => {
-//   const [user, setValue] = useState(initialValue);
-//   const updateUser = useCallback(() => {
-// 		alert(JSON.stringify(user))
-// 		setValue({
-// 			...user,
-// 			testing: "yes"
-// 		}), [user]
-// 	});
-//   return { user, updateUser };
-// }
-
-// function () {
-//   const { user, updateUser } = userComponent();
-//   return (
-//     <div>
-//       {user.name} {user.testing}
-//       <button onClick={updateUser}>Update User</button>
-//     </div>
-//   );
-// }
-
-export default () => (
-	<div class="is-pulled-right">
-		<UserComponent />
-	</div>
-)
+export default () => {
+	return (
+		<div class="is-pulled-right">
+			<div class="media">
+				<UserMeta />
+				<UserUI />
+			</div>
+		</div>
+	)
+}
