aa688021e0c9eb208fc9a4dbc429c550ffb980b8
Adam Braimbridge <adam.braimbridge@ft.com>
2019-09-16 22:11:10 +0100

    manifest

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,7 +2,7 @@ import Authentication from '../authentication'
 import style from './style'
 
 export default () => (
-	<nav class="section card is-mobile" role="navigation" aria-label="main navigation">
+	<nav id="primary-nav" class="section card is-mobile" role="navigation" aria-label="main navigation">
 		<Authentication />
 		<div>💀 goldbug.club</div>
 	</nav>
