8558c765f1caf80b94169fa8e49be5a490574f99
Adam Braimbridge <adam.braimbridge@ft.com>
2019-09-16 11:17:08 +0100

    Update index.js

--- src/components/authentication/index.js
+++ src/components/authentication/index.js
@@ -1,4 +1,4 @@
-import { useReducer } from 'preact/hooks'
+import { useState, useCallback, useEffect } from 'preact/hooks'
 
 /**
  * Authenticate against Google via Netlify.
@@ -22,7 +22,7 @@ const getAuthenticationFromHash = () => {
 					}, {})
 			: undefined
 		if (authenticationData) {
-			goTrueAuth
+			return goTrueAuth
 				.createUser(authenticationData, true)
 				.then(authenticatedUser => authenticatedUser)
 				.catch(console.error)
@@ -33,7 +33,7 @@ const getAuthenticationFromHash = () => {
 }
 
 /**
- * Local storage trumps URL hash. To sign out local user, the Sign Out UI must be used.
+ * GoTrue local storage trumps URL hash. To sign out local user, the Sign Out UI must be used.
  */
 const getAuthenticatedUser = () => {
 	let currentUser = goTrueAuth.currentUser()
@@ -43,76 +43,88 @@ const getAuthenticatedUser = () => {
 
 	// Remove tokens from hash so that token does not remain in browser history.
 	history.replaceState(null, null, '/')
-
-	return currentUser
+	return currentUser || false
 }
 
-const anonUser = { localUser: false, buttonText: 'Google Sign In' }
+const getLocalUser = () => {
+	const initialUserState = () => {
+		const authenticatedUser = getAuthenticatedUser()
+		return authenticatedUser ? authenticatedUser : false
+	}
 
-const initialUserState = () => {
-	const authenticatedUser = getAuthenticatedUser()
-	if (authenticatedUser) {
-		return {
-			localUser: authenticatedUser,
-			buttonText: 'Sign Out',
-		}
-	} else {
-		return anonUser
+	// Todo: If localuser has been set in state already, does this return from state, or from initialUserState?
+	const [localUser] = useState(initialUserState)
+	return localUser
+}
+
+const UserMeta = () => {
+	const localUser = getLocalUser()
+	if (localUser) {
+		const { avatar_url, full_name } = localUser.user_metadata
+		return (
+			<Fragment>
+				<figure class="media-left image is-24x24">
+					<img class="is-rounded" src={avatar_url} />
+				</figure>
+				<div class="media-content">{full_name}</div>
+			</Fragment>
+		)
 	}
 }
 
-const updateUserState = (state, action) => {
-	// updateUser() is triggered by the Sign In / Sign Out button.
-	if (action === 'updateUser') {
-		const { localUser } = state || {}
-		if (!localUser) {
-			// User does not exist, so they must have clicked "Sign In"
-			// Redirect to OAuth endpoint to sign in.
-			location = 'https://www.goldbug.club/.netlify/identity/authorize?provider=google'
-		} else {
-			// User exists, so they must have clicked "Sign Out"
-
-			// todo: doublecheck this promise
-			return localUser
-				.logout()
-				.then(() => {
-					console.log('Signed out.')
-					return anonUser
-				})
-				.catch(console.error)
-		}
+const UserUI = () => {
+	const initialButtonState = () => {
+		const localUser = getLocalUser()
+		return localUser ? 'Sign Out' : 'Google Sign In'
 	}
-}
+	const [buttonText, setValue] = useState(initialButtonState)
+	const handleClick = useCallback(() => {
+		const localUser = getLocalUser()
+		setValue(localUser ? 'Sign Out' : 'Google Sign In')
 
-const UserComponent = () => {
-	const [state, dispatch] = useReducer(updateUserState, initialUserState)
-	const { localUser, buttonText } = state || {}
+		// User does not exist, so they must have clicked "Sign In"
+		// Redirect to OAuth endpoint.
+		location = 'https://www.goldbug.club/.netlify/identity/authorize?provider=google'
 
-	const UserMeta = () => {
-		if (localUser) {
-			const { avatar_url, full_name } = localUser.user_metadata
-			return (
-				<Fragment>
-					<figure class="media-left image is-24x24">
-						<img class="is-rounded" src={avatar_url} />
-					</figure>
-					<div class="media-content">{full_name}</div>
-				</Fragment>
-			)
-		}
-	}
+	}, [buttonText])
+
+	// Todo: Will useEffect() handle logging out / redirecting to oauth?
+	useEffect(() => {
+		console.log({ buttonText })
+	}, [buttonText])
+
 	return (
-		<div class="is-pulled-right">
-			<div class="media">
-				<UserMeta />
-				<div class="media-right">
-					<button class="button is-small" onClick={() => dispatch('updateUser')}>
-						{buttonText}
-					</button>
-				</div>
-			</div>
+		<div class="media-right">
+			<button class="button is-small" onClick={handleClick}>
+				{buttonText}
+			</button>
 		</div>
 	)
 }
 
-export default () => <UserComponent />
+export default () => (
+	<div class="is-pulled-right">
+		<div class="media">
+			<UserMeta />
+			<UserUI />
+		</div>
+	</div>
+)
+
+// // Todo: Get local user from state
+// if (!localUser) {
+// 	// User does not exist, so they must have clicked "Sign In"
+// 	// Redirect to OAuth endpoint.
+// 	location = 'https://www.goldbug.club/.netlify/identity/authorize?provider=google'
+// } else {
+// 	// User exists, so they must have clicked "Sign Out"
+
+// 	// todo: doublecheck this promise
+// 	return localUser
+// 		.logout()
+// 		.then(() => {
+// 			console.log('Signed out.')
+// 			return anonUser
+// 		})
+// 		.catch(console.error)
+// }
