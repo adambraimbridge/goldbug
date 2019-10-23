549ad269130754613544ea370ed127330c52bb32
Adam Braimbridge <adam.braimbridge@ft.com>
2019-10-23 06:49:58 +0100

    src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App.js'
+import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 
 import './style/index.scss'
