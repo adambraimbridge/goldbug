2cd9e796fb45ecb9028341750fff8dc3e932e4b0
Adam Braimbridge <adam.braimbridge@ft.com>
2019-11-03 10:57:03 +0000

    Cloudant

--- functions/get-cloudant-database.js
+++ functions/get-cloudant-database.js
@@ -1,42 +1,69 @@
-const Cloudant = require('@cloudant/cloudant')
-const querystring = require('querystring')
-exports.handler = async ({ httpMethod, body, ...rest }) => {
-	console.log({ rest })
-	console.log(querystring.parse(body))
+import Cloudant from '@cloudant/cloudant'
+import querystring from 'querystring'
+
+export default async (event, context) => {
+	console.log({ event, context })
+
+	const { httpMethod, body } = event
 
 	if (httpMethod !== 'POST') {
 		return { statusCode: 405, body: 'Method Not Allowed' }
 	}
 
-	const cloudant = await Cloudant({
-		username: process.env.CLOUDANT_USERNAME,
-		password: process.env.CLOUDANT_PASSWORD,
-		url: `https://${process.env.CLOUDANT_USERNAME}.cloudantnosqldb.appdomain.cloud/`,
-	})
-	const databases = await cloudant.db.list()
-	console.log({ databases })
+	const params = querystring.parse(body)
+	const name = params.name || 'World'
 
-	return {
-		body: databases,
-		statusCode: 200,
+	console.log({ params })
+
+	try {
+		const cloudant = await Cloudant({
+			username: process.env.CLOUDANT_USERNAME,
+			password: process.env.CLOUDANT_PASSWORD,
+			url: `https://${process.env.CLOUDANT_USERNAME}.cloudantnosqldb.appdomain.cloud/`,
+		})
+		const databases = await cloudant.db.list()
+		console.log({ databases })
+		return { statusCode: 200, body: databases, headers: { 'Content-Type': 'application/json' } }
+	} catch (error) {
+		return { statusCode: 500, body: String(error) }
 	}
 }
 
-// /* Global configuration */
-// ;(async (user, context, callback) => {
-//   Cloudant({
-//     username: process.env.CLOUDANT_USERNAME,
-//     password: process.env.CLOUDANT_PASSWORD,
-//     url: `https://${process.env.CLOUDANT_USERNAME}.cloudantnosqldb.appdomain.cloud/`
-//   }, (error, cloudant, pong) => {
-//     if (error) console.error(`ERROR: ${error.message}`)
-//     cloudant.db.list(function (err, body, headers) {
-//       console.log({ err, body, headers });
-//     })
-// })()
+// exports.handler = async ({ httpMethod, body, ...rest }) => {
 
-// const { token, text, response_url } = querystring.parse(body)
-// const { SLACK_TOKEN } = process.env
-// if (!token || token !== SLACK_TOKEN) {
-//   return { statusCode: 401, body: 'Unauthorized' }
+// 	if (httpMethod !== 'POST') {
+// 		return { statusCode: 405, body: 'Method Not Allowed' }
+// 	}
+
+// 	const cloudant = await Cloudant({
+// 		username: process.env.CLOUDANT_USERNAME,
+// 		password: process.env.CLOUDANT_PASSWORD,
+// 		url: `https://${process.env.CLOUDANT_USERNAME}.cloudantnosqldb.appdomain.cloud/`,
+// 	})
+// 	const databases = await cloudant.db.list()
+// 	console.log({ databases })
+
+// 	return {
+// 		body: databases,
+// 		statusCode: 200,
+// 	}
 // }
+
+// // /* Global configuration */
+// // ;(async (user, context, callback) => {
+// //   Cloudant({
+// //     username: process.env.CLOUDANT_USERNAME,
+// //     password: process.env.CLOUDANT_PASSWORD,
+// //     url: `https://${process.env.CLOUDANT_USERNAME}.cloudantnosqldb.appdomain.cloud/`
+// //   }, (error, cloudant, pong) => {
+// //     if (error) console.error(`ERROR: ${error.message}`)
+// //     cloudant.db.list(function (err, body, headers) {
+// //       console.log({ err, body, headers });
+// //     })
+// // })()
+
+// // const { token, text, response_url } = querystring.parse(body)
+// // const { SLACK_TOKEN } = process.env
+// // if (!token || token !== SLACK_TOKEN) {
+// //   return { statusCode: 401, body: 'Unauthorized' }
+// // }
