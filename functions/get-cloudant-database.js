e16398ea0e7fff81c23d5ff7cef727ab5026c25b
Adam Braimbridge <adam.braimbridge@ft.com>
2019-11-05 22:21:57 +0000

    Fiddly

--- functions/get-cloudant-database.js
+++ functions/get-cloudant-database.js
@@ -1,39 +1,33 @@
-// const Cloudant = require('@cloudant/cloudant')
-// const querystring = require('querystring')
+const Cloudant = require('@cloudant/cloudant')
+const querystring = require('querystring')
 
-exports.handler = async request => {
-	return { statusCode: 204 }
+exports.handler = async (event, context) => {
+	console.log({ event, context })
+
+	const { httpMethod, body } = event
+	if (httpMethod !== 'POST') {
+		return { statusCode: 405, body: 'Method Not Allowed' }
+	}
+
+	try {
+		const params = querystring.parse(body)
+		console.log({ params })
+
+		const cloudant = await Cloudant({
+			username: process.env.CLOUDANT_USERNAME,
+			password: process.env.CLOUDANT_PASSWORD,
+			url: `https://${process.env.CLOUDANT_USERNAME}.cloudantnosqldb.appdomain.cloud/`,
+		})
+		const databases = await cloudant.db.list()
+		console.log({ databases })
+
+		if (!databases.length) {
+			throw new Error('No databases found.')
+		}
+
+		return { statusCode: 200, body: JSON.stringify(event), headers: { 'Content-Type': 'application/json' } }
+	} catch (error) {
+		console.error({ error })
+		return { statusCode: 500, body: String(error) }
+	}
 }
-// const { httpMethod, body } = request
-// const { payload } = querystring.parse(body)
-
-// console.log({ httpMethod, payload, request })
-
-// if (httpMethod !== 'POST') {
-// 	return { statusCode: 405, body: 'Method Not Allowed' }
-// }
-
-// const params = querystring.parse(body)
-
-// console.log({ params })
-
-// return { statusCode: 200, body: JSON.stringify(request), headers: { 'Content-Type': 'application/json' } }
-// try {
-// 	const cloudant = await Cloudant({
-// 		username: process.env.CLOUDANT_USERNAME,
-// 		password: process.env.CLOUDANT_PASSWORD,
-// 		url: `https://${process.env.CLOUDANT_USERNAME}.cloudantnosqldb.appdomain.cloud/`,
-// 	})
-// 	const databases = await cloudant.db.list()
-// 	console.log({ databases })
-
-// 	if (!databases.length) {
-// 		throw new Error('No databases found.')
-// 	}
-
-// 	return { statusCode: 200, body: JSON.stringify(request), headers: { 'Content-Type': 'application/json' } }
-// } catch (error) {
-// 	console.error({ error })
-// 	return { statusCode: 500, body: String(error) }
-// }
-// }
