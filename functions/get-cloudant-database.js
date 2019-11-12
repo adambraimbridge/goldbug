e1809eb9d0f7c5291e8698a6b881fdf60242a8b1
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2019-11-12 15:33:53 +0000

    newappmeta

--- functions/get-cloudant-database.js
+++ functions/get-cloudant-database.js
@@ -38,26 +38,26 @@ exports.handler = async event => {
 		console.log(`Database not found for ${id}. Provisioning ...`)
 	}
 
-	/**
-	 * Create a new database for the user and generate API key/password credentials.
-	 */
-	try {
-		await cloudant.db.create(id)
-		remoteDatabase = await cloudant.db.get(id)
-		const newCredentials = await getDatabaseCredentials(cloudant, remoteDatabase)
-		const { app_metadata } = payload.user
-		const newAppMetadata = Object.assign({}, app_metadata, { credentials: newCredentials })
-		const body = JSON.stringify({
-			app_metadata: newAppMetadata,
-		})
+	// /**
+	//  * Create a new database for the user and generate API key/password credentials.
+	//  */
+	// try {
+	// 	await cloudant.db.create(id)
+	// 	remoteDatabase = await cloudant.db.get(id)
+	// 	const newCredentials = await getDatabaseCredentials(cloudant, remoteDatabase)
+	// 	const { app_metadata } = payload.user
+	// 	const newAppMetadata = Object.assign({}, app_metadata, { credentials: newCredentials })
+	// 	const body = JSON.stringify({
+	// 		app_metadata: newAppMetadata,
+	// 	})
 
-		console.log({ body })
+	// 	console.log({ body })
 
-		// Save the credentials in the Netlify user's app_metadata.
-		// See: https://docs.netlify.com/functions/functions-and-identity/#trigger-serverless-functions-on-identity-events
-		return { statusCode: 200, body }
-	} catch (error) {
-		console.error({ error })
-		return { statusCode: 500, body: String(error) }
-	}
+	// 	// Save the credentials in the Netlify user's app_metadata.
+	// 	// See: https://docs.netlify.com/functions/functions-and-identity/#trigger-serverless-functions-on-identity-events
+	// 	return { statusCode: 200, body }
+	// } catch (error) {
+	// 	console.error({ error })
+	// 	return { statusCode: 500, body: String(error) }
+	// }
 }
