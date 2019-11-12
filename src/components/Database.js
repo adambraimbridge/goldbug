22ca5bbe84ba64a363986c8dfee2e1226ba8ad30
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2019-11-12 10:30:23 +0000

    id

--- src/components/Database.js
+++ src/components/Database.js
@@ -9,9 +9,9 @@ localDatabase.changes({
 
 const syncRemoteDatabase = localUser => {
 	console.log({ localUser })
-
+	const { id } = localUser
 	const { key, password } = localUser.app_metadata.credentials
-	const remoteUrl = `https://${key}:${password}@${CLOUDANT_USERNAME}.cloudantnosqldb.appdomain.cloud/`
+	const remoteUrl = `https://${key}:${password}@${CLOUDANT_USERNAME}.cloudantnosqldb.appdomain.cloud/${id}`
 	const remoteDatabase = new PouchDB(remoteUrl)
 
 	localDatabase
