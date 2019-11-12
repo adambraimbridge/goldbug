383df34160df9c47761f580fc5f2b9a276ce114e
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2019-11-12 19:05:54 +0000

    bodyString

--- src/components/Database.js
+++ src/components/Database.js
@@ -8,7 +8,8 @@ localDatabase.changes({
 })
 
 const syncRemoteDatabase = async localUser => {
-	console.log({ localUser })
+	if (!localUser) return
+
 	const { id } = localUser
 	const { key, password } = localUser.app_metadata.credentials
 	const remoteUrl = `https://${key}:${password}@${CLOUDANT_USERNAME}.cloudantnosqldb.appdomain.cloud/${id}`
