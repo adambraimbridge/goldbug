ff4e9c385fc9a4ebc847402c3d69946183e354eb
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2019-11-14 20:06:36 +0000

    Dublin airport

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,4 @@
-import React, { useState, useLayoutEffect } from 'react'
+import React, { useState, useLayoutEffect, useEffect } from 'react'
 import { syncRemoteDatabase, addMessage, removeMessage } from './Database'
 
 const Message = ({ text, userMeta }) => {
