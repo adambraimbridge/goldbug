9fa2999d07a3c0c20d48def3c04dd4345a9f0e13
Adam Braimbridge <adam.braimbridge@ft.com>
2019-12-16 17:35:15 +0000

    Switched from Gatsby to NextJs

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import { Context } from './Context'
+import { Switch, Route } from 'react-router-dom'
 import { AuthenticationUI, AuthenticationPanel } from './AuthenticationUI'
 import { Chat } from './Chat'
 import { Workshop } from './Workshop'
@@ -34,19 +34,17 @@ export const App = () => {
 	const { loading } = state || false
 	const loadingClassName = `test spinner-border spinner-border-sm ${loading ? '' : 'd-none'}`
 	return (
-		<Router>
-			<div id="layout" className="full-height max-with p-0 m-0 mx-auto">
-				<nav className="px-0 py-2 justify-content-between navbar navbar-expand navbar-dark">
-					<Link to="/" className="centered">
-						<img alt="💀" src="/icons/favicon.png" className="align-baseline icon" />
-						Goldbug Club
-						<noscript>Welcome to Goldbug Club. Thanks for visiting. Please enable JavaScript.</noscript>
-					</Link>
-					<span id="loading" className={loadingClassName} role="status" aria-hidden={loading}></span>
-					<AuthenticationUI />
-				</nav>
-				<Page />
-			</div>
-		</Router>
+		<div id="layout" className="full-height max-with p-0 m-0 mx-auto">
+			<nav className="px-0 py-2 justify-content-between navbar navbar-expand navbar-dark">
+				<a href="/" className="centered">
+					<img alt="💀" src="/icons/favicon.png" className="align-baseline icon" />
+					Goldbug Club
+					<noscript>Welcome to Goldbug Club. Thanks for visiting. Please enable JavaScript.</noscript>
+				</a>
+				<span id="loading" className={loadingClassName} role="status" aria-hidden={loading}></span>
+				<AuthenticationUI />
+			</nav>
+			<Page />
+		</div>
 	)
 }
--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -10,15 +10,17 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
-const isLocalhost = Boolean(
-	window.location.hostname === 'localhost' ||
-		// [::1] is the IPv6 localhost address.
-		window.location.hostname === '[::1]' ||
-		// 127.0.0.1/8 is considered localhost for IPv4.
-		window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
-)
-
 export function register(config) {
+	if (!process.browser) return
+
+	const isLocalhost = Boolean(
+		window.location.hostname === 'localhost' ||
+			// [::1] is the IPv6 localhost address.
+			window.location.hostname === '[::1]' ||
+			// 127.0.0.1/8 is considered localhost for IPv4.
+			window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+	)
+
 	if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
 		// The URL constructor is available in all browsers that support SW.
 		const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href)
@@ -114,6 +116,8 @@ function checkValidServiceWorker(swUrl, config) {
 }
 
 export function unregister() {
+	if (!process.browser) return
+
 	if ('serviceWorker' in navigator) {
 		navigator.serviceWorker.ready.then(registration => {
 			registration.unregister()
