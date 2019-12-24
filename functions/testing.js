ada666c41f2bf07eea1c9d506240fdb6225b10cd
Adam Braimbridge <adam.braimbridge@ft.com>
2019-12-24 14:33:42 +0000

    xmas

--- functions/testing.js
+++ functions/testing.js
@@ -6,17 +6,16 @@ const axios = require('axios')
 
 exports.handler = async (payload, context) => {
 	try {
-		const { body } = payload
-		const { id } = JSON.parse(body)
+		// const { body } = payload
+		// const { id } = JSON.parse(body)
 
 		const userToken = payload.headers['x-nf-sign']
 		const decoded = jwt.verify(userToken, process.env.API_SIGNATURE_TOKEN)
-		if (!decoded) throw Error('Invalid User Token.')
-
 		console.log({ decoded })
 
 		const url = context.clientContext.identity.url
 		const token = context.clientContext.identity.token
+		console.log({ url, token })
 
 		// const response = await axios(`${url}/admin/users/${id}`, {
 		// 	method: 'GET',
@@ -26,7 +25,7 @@ exports.handler = async (payload, context) => {
 
 		return {
 			statusCode: 200,
-			body: JSON.stringify({ url, token }),
+			body: JSON.stringify({ context }),
 		}
 	} catch (error) {
 		return { statusCode: 500, body: error }
