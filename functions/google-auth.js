386595ac5ea0f8220ecaf43ebf36f2071e14b403
Adam Braimbridge <adam.braimbridge@ft.com>
2020-01-03 22:00:45 +0000

    google-auth

--- functions/google-auth.js
+++ functions/google-auth.js
@@ -5,10 +5,24 @@
  * https://github.com/googleapis/google-api-nodejs-client#oauth2-client
  */
 const { google } = require('googleapis')
+const oauth2Client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_REDIRECT_URL)
+const homepageUrl = 'https://www.goldbug.com'
 
 exports.handler = async (payload, context) => {
-	try {
-		const oauth2Client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_REDIRECT_URL)
+	const { queryStringParameters } = payload || {}
+	if (queryStringParameters.code) {
+		// This will provide an object with the access_token and refresh_token.
+		// Save these somewhere safe so they can be used at a later time.
+		const { tokens } = await oauth2Client.getToken(code)
+		oauth2Client.setCredentials(tokens)
+
+		console.log({ tokens })
+
+		return {
+			statusCode: 303,
+			headers: { location: `${homepageUrl}?tokens=${JSON.stringify(tokens)}` },
+		}
+	} else {
 		const oAuthUrl = oauth2Client.generateAuthUrl({
 			// 'online' (default) or 'offline' (gets refresh_token)
 			access_type: 'offline',
@@ -21,12 +35,5 @@ exports.handler = async (payload, context) => {
 			statusCode: 303,
 			headers: { location: oAuthUrl },
 		}
-
-		// return {
-		// 	statusCode: 200,
-		// 	body: JSON.stringify({ oAuthUrl, payload, context }),
-		// }
-	} catch (error) {
-		return { statusCode: 500, body: error }
 	}
 }
