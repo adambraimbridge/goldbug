51fbb697e167c04adcc1fbec28d6b8b591c14cf4
Adam Braimbridge <adam.braimbridge@ft.com>
2020-01-07 19:15:58 +0000

    google-auth

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,14 @@ import { App } from '../components/App'
 import { unregister as unregisterServiceWorker } from '../serviceWorker'
 import '../style/index.scss'
 export default () => (
-	<ContextProvider>
-		<App />
-	</ContextProvider>
+	<>
+		<meta name="google-signin-scope" content="profile email" />
+		<meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com" />
+		<script src="https://apis.google.com/js/platform.js" async defer></script>
+		<ContextProvider>
+			<App />
+		</ContextProvider>
+	</>
 )
 
 // Service workers: https://bit.ly/CRA-PWA
