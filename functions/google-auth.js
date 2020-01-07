5df8391f2e386bf4c35695f691056031a1ad5fcd
Adam Braimbridge <adam.braimbridge@ft.com>
2020-01-07 19:04:53 +0000

    google-auth

--- functions/google-auth.js
+++ functions/google-auth.js
@@ -8,6 +8,19 @@ const { google } = require('googleapis')
 const oauth2Client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_REDIRECT_URL)
 const HOMEPAGE_URL = 'https://www.goldbug.com'
 
+const verify = async () => {
+	const ticket = await client.verifyIdToken({
+		idToken: token,
+		audience: process.env.GOOGLE_CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+		// Or, if multiple clients access the backend:
+		//[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+	})
+	const payload = ticket.getPayload()
+	const userid = payload['sub']
+	// If request specified a G Suite domain:
+	//const domain = payload['hd'];
+}
+
 const gotoGoogleAuth = () => {
 	const oAuthUrl = oauth2Client.generateAuthUrl({
 		access_type: 'offline',
@@ -28,6 +41,8 @@ exports.handler = async payload => {
 		// At this point the user has authenticated with Google.
 		const { tokens } = await oauth2Client.getToken(queryStringParameters.code)
 		// oauth2Client.setCredentials(tokens)
+
+		verify()
 
 		// Google's supplied the user's JSON token,
 		// so save them (somehow), then redirect to the home page
