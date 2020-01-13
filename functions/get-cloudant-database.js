e0a5b222420717448de43673165b96a4f0a62a96
Adam Braimbridge <adam.braimbridge@ft.com>
2020-01-13 20:37:59 +0000

    google-auth

--- functions/get-cloudant-database.js
+++ functions/get-cloudant-database.js
@@ -117,6 +117,11 @@ exports.handler = async payload => {
 		body: JSON.stringify(credentials),
 		headers: {
 			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Methods': '*',
+			'Access-Control-Max-Age': '2592000',
+			'Access-Control-Allow-Credentials': 'true',
 		},
 	}
 	console.log('Returning response data', { data })
