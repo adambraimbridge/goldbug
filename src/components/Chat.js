1c26062f9d1cb349d8c7874c88230860e731ded3
Adam Braimbridge <adam.braimbridge@ft.com>
2020-02-02 16:42:05 +0000

    refreshMessagesState

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -54,6 +54,7 @@ export const Chat = () => {
 
 	useLayoutEffect(() => {
 		const refreshMessagesState = allDocs => {
+			console.log({ allDocs })
 			const sanitisedMessages = allDocs.rows
 				.filter(row => {
 					return row.doc && row.doc._id && row.doc.value && row.doc.name && row.doc.imageUrl
